795bd82f5c55e334a2544f0733e86b93bedde6f4
Pradeish Misara R <100019740+pradeish29@users.noreply.github.com>
2023-05-25 11:32:32 +0530

    Update script.js

--- module4/script.js
+++ module4/script.js
@@ -1,16 +1,15 @@
 (function () {
-
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-for (var i = 0; i < names.length; i++) {
+for (var i = 0; i < names.length; i++)
+{
   var firstLetter = names[i].charAt(0).toLowerCase();
-
-  if (firstLetter === 'j') {
+  if (firstLetter === 'j') 
+  {
     byeSpeaker(names[i]);
   }
-  else {
+  else 
+  {
     helloSpeaker(names[i]);
   }
 }
-
 })();
